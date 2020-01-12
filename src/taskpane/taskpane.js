af54588edfa012a4ac9d58e91a36f8a504c7917c
Zanghi' Antonino <a.zanghi@almaviva.it>
2020-01-12 23:49:26 +0100

    29th release

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,7 +7,7 @@
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
+    //document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
   }
